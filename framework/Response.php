ba76c37782280df5df91cae0595119d130f99757
AdrianoLima19 <adrianolima.dev@gmail.com>
2024-10-27 18:21:49 -0300

    Adiciona métodos a classe Response

--- framework/Response.php
+++ framework/Response.php
@@ -4,5 +4,66 @@ namespace eNote;
 
 class Response
 {
-    // code...
+    protected string $content;
+    protected int $status;
+    protected array $headers;
+
+    public function __construct(string $content = '', int $status = 200, array $headers = [])
+    {
+        $this->content = $content;
+        $this->status = $status;
+        $this->headers = $headers;
+    }
+
+    public function json(array|object $data, int $status = 200): self
+    {
+        $this->content = json_encode($data);
+        $this->status = $status;
+        $this->headers['Content-Type'] = 'application/json';
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    public function setHeaders(array $headers): self
+    {
+        $this->headers = $headers;
+        return $this;
+    }
+
+    public function send()
+    {
+        http_response_code($this->status);
+
+        foreach ($this->headers as $key => $value) {
+            header("$key: $value");
+        }
+
+        echo $this->content;
+    }
 }
